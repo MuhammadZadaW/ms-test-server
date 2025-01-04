8e4c404cd600f96c81ccb7a660658f3a1ddedafa
Muhammad Zada Widiyanto <muhammad.zada.w@gmail.com>
2025-01-04 19:09:30 +0700

    add no routes

--- main.go
+++ main.go
@@ -3,15 +3,28 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"github.com/gin-gonic/gin"
 )
 
 func main () {
+
+	fmt.Println("Tocal CPU", runtime.NumCPU())
+
 	r := gin.Default()
+	
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{
+			"message": "routes not found",
+		})
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.Run()
 }
